72bf9ac334b67d903a379505f4d797e7c28aed3f
zhengji <zhengji@youmi.net>
2015-08-26 17:44:36 +0800

    add unittest file

--- monitor_test.go
+++ monitor_test.go
@@ -6,17 +6,16 @@ package monitor
 
 import (
 	"fmt"
-	"github.com/zheng-ji/gosvr-monitor"
 	"testing"
 	"time"
 )
 
 func TestMonitor(t *testing.T) {
 	fmt.Println("start")
-	monitor.InitMonitor([]string{"WRITE", "READ"}, 1)
-	monitor.StartMonitorServer("0.0.0.0:7070")
+	InitMonitor([]string{"WRITE", "READ"}, 1)
+	StartMonitorServer("0.0.0.0:7070")
 	timeStart := time.Now()
 	time.Sleep(300 * time.Millisecond)
-	monitor.StatByAction("READ", timeStart)
+	StatByAction("READ", timeStart)
 	fmt.Println("end")
 }
